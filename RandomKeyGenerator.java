37ecdb2976a58f8396dfe61d2f081dcf6d0729eb
alisajids <34768896+alisajids@users.noreply.github.com>
2023-04-17 16:41:23 +0530

    Create RandomKeyGenerator.java

---
+++ RandomKeyGenerator.java
@@ -0,0 +1,15 @@
+
+import java.security.SecureRandom;
+
+public class RandomKeyGenerator{
+
+	static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%&*()_+-{}[]/?<>;";
+	static SecureRandom rnd = new SecureRandom();
+	
+	public static String randomString( int len ){
+		   StringBuilder sb = new StringBuilder( len );
+		   for( int i = 0; i < len; i++ ) 
+		      sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
+		   return sb.toString();
+	}
+}
