ed9fe7e28294683fb73070416f7f930376352e5d
SAJID ALI <34768896+alisajids@users.noreply.github.com>
2024-08-14 21:17:52 +0530

    Update DataSourceConfig.java

--- config/DataSourceConfig.java
+++ config/DataSourceConfig.java
@@ -1,1 +1,34 @@
+package config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Data
+@EnableConfigurationProperties({OracleConfig.class})
+@EnableTransactionManagement  
+public class DataSourceConfig{
+
+  @Primary
+  @Bean(name = "oracle")
+  public DataSource oracleDataSource(OracleConfig oracleConfig) {
+	  log.debug("url:{},user:{}",oracleConfig.getUrl()
+			  ,oracleConfig.getUserName());
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl(oracleConfig.getUrl());
+    config.setUsername(oracleConfig.getUserName());
+    config.setPassword(oracleConfig.getPassword());
+    config.setDriverClassName(oracleConfig.getDriverClassName());
+    return new HikariDataSource(config);
+  }
+}
