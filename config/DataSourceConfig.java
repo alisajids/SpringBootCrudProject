a798427977cd77faf051fe966ee9e7ea42606e53
SAJID ALI <34768896+alisajids@users.noreply.github.com>
2024-08-14 21:36:01 +0530

    Update DataSourceConfig.java

--- config/DataSourceConfig.java
+++ config/DataSourceConfig.java
@@ -17,6 +17,11 @@ import javax.sql.DataSource;
 @Data
 @EnableConfigurationProperties({OracleConfig.class})
 @EnableTransactionManagement  
+//if more than one DB configured then specify basePackage, entityManagerref and transactionManagerRef
+@EnableJpaRepositories(
+ basePackages = "com.entity",
+ entityManagerFactoryRef = "oracleEntityManagerFactory",
+ transactionManagerRed = "oracleTransactionManager")
 public class DataSourceConfig{
 
   @Primary
@@ -31,4 +36,23 @@ public class DataSourceConfig{
     config.setDriverClassName(oracleConfig.getDriverClassName());
     return new HikariDataSource(config);
   }
+@Primary
+@Bean(name = "oracleEntityManagerFactory")
+public LocalContainerEntityManagerFactoryBean oracleEntityManagerFactory(
+	@Qualifier("oracle") DataSource datasource){
+
+	LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+	em.setDataSource(datasource);
+	em.setPackagesToScan(new String[] { "com.entity" });
+	JpaVendorAdapter jpaVendorAdapter = new HbernateJpaVendorAdaptor();
+	em.setJpaVendorAdapter(jpaVendorAdapter);
+	return em;
+}
+
+@Primary
+@Bean(name = "oracleEntityManagerFactory")
+public PlatformTransactionManager oracleTransactionManager(@Qualifier("oracleEntityManagerFactory") EntityManagerFactory entityManagerFactory){
+  return new JpaTransactionManager(entityManagerFactory);
+}	
+	 
 }
